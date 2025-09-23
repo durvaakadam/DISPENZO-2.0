199e7dff4067afa95e9591fe21e2ab018f4bf920
durvaakadam <durvakadam204@gmail.com>
2025-09-23 14:32:57 +0530

    lcd code

---
+++ lcd.ino
@@ -0,0 +1,15 @@
+#include <LiquidCrystal.h>
+
+// RS, E, D4, D5, D6, D7
+LiquidCrystal lcd(21, 22, 19, 18, 5, 4);
+
+void setup() {
+  lcd.begin(16, 2);          // initialize the 16x2 LCD
+  lcd.print("DURVA KADAM!"); // print message on first row
+  lcd.setCursor(0, 1);       // move to second row
+  lcd.print("SACHI JADHAV!");
+}
+
+void loop() {
+  // nothing here, static message
+}
